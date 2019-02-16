9431e0b2ec10b4187095fe4827d2300cae93e145
Ville M. Vainio <ville.vainio@basware.com>
2019-02-16 17:49:50 +0200

    remove fluff

--- rll/RllApp.cs
+++ rll/RllApp.cs
@@ -24,44 +24,8 @@ namespace Rll
         static List<string[]> Highlighters = new List<string[]>();
         private static Interpreter AppInterpreter;
 
-        static void PushOutput(string o)
-        {
-            var col = RllConfig.TruncateCol;
-            var toAdd = col == 0 || o.Length < col ? o : o.Substring(0, col) + "...";
-            // let's add original to summary
-            OutputLines.Add(o);
-            if (RllConfig.ShowOnly != null && !RllConfig.ShowOnly.Any(p => o.Contains(p))) {
-                return;
-            } 
-            Console.WriteLine(toAdd);
-        }
-        public static void InteractProcess(Process p)
-        {
-            p.StartInfo.UseShellExecute = false;
-            p.OutputDataReceived += (o, d) =>
-            {
-                if (d.Data != null)
-                {
-                    PushOutput(d.Data);
-                }
-            };
-            p.ErrorDataReceived += (o, d) =>
-            {
-                if (d.Data != null)
-                {
-                    PushOutput(d.Data);
-                    ErrorLines.Add(d.Data);
-                }
-            };
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.RedirectStandardError = true;
-            p.Start();
-            p.BeginOutputReadLine();
-            p.BeginErrorReadLine();
-        }
-
         // rll-run
-        public static Process ConvenienceRun(string cmd, string arg, string cwd, bool interact)
+        public static Process ConvenienceRun(string cmd, string arg, string cwd)
         {
             var p = new Process();
             var psi = p.StartInfo;
@@ -72,13 +36,7 @@ namespace Rll
             {
                 psi.WorkingDirectory = cwd;
             }
-            if (interact)
-            {
-                InteractProcess(p);
-            } else
-            {
-                p.Start();
-            }
+            p.Start();
             return p;
         }
 
@@ -88,37 +46,10 @@ namespace Rll
         {
             Interpreter.CreateSymbolTableDelegate appExt = _ => new Dictionary<Symbol, object>()
             {
-                { Symbol.FromString("highlight-add"), NativeProcedure.Create<string,List<object>, object>((group, l) =>
-                {
-
-                    Highlighters.Add( new[] { group }.Concat(l.Cast<string>()).ToArray());
-                    return None.Instance;
-                }) },
-                { Symbol.FromString("highlight-show"), NativeProcedure.Create(() =>
-                {
-                    foreach (var h in Highlighters)
-                    {
-                        var toSearch = h.Skip(1);
-                        var found = OutputLines.Where(line => toSearch.Any(subs => line.Contains(subs))).ToArray();
-                        if (found.Length >0)
-                        {
-                            Console.WriteLine(h[0]);
-                            foreach (var l in found) {
-                                Console.WriteLine(l);
-                            }
-
-                        }
-                    };
-                    return None.Instance;
-                }) },
-                { Symbol.FromString("cf-truncate"), NativeProcedure.Create<int, object>(num => RllConfig.TruncateCol = num ) },
                 { Symbol.FromString("repl"), NativeProcedure.Create(() => { RunRepl(); return None.Instance; }) },
 
-                { Symbol.FromString("cf-show-only"), NativeProcedure.Create<List<object>, object>(patterns => RllConfig.ShowOnly = patterns.Cast<string>().ToArray()) },
-                { Symbol.FromString("rll-run-capture"), NativeProcedure.Create<string, string, Process>((cmd, arg) =>
-                    ConvenienceRun(cmd, arg, null, true)) },
                 { Symbol.FromString("os-system"), NativeProcedure.Create<string, string, Process >((cmd, arg) =>
-                    ConvenienceRun(cmd, arg, null, false)) },
+                    ConvenienceRun(cmd, arg, null)) },
                 { Symbol.FromString("unzip"), NativeProcedure.Create<string, string, None >((zipfile, targetdir) =>
                 {
                     ZipFile.ExtractToDirectory(zipfile, targetdir);
@@ -131,30 +62,6 @@ namespace Rll
                     return None.Instance;
                 }) },
 
-                // I don't know whether I'll remove these
-                { Symbol.FromString("psi-exe"), NativeProcedure.Create<ProcessStartInfo, string, object>((psi, s) => psi.FileName = s) },
-                { Symbol.FromString("psi-arg"), NativeProcedure.Create<ProcessStartInfo, string, object>((psi, s) => psi.Arguments = s) },
-                { Symbol.FromString("psi-dir"), NativeProcedure.Create<ProcessStartInfo, string, object>((psi, s) => psi.WorkingDirectory = s) },
-                { Symbol.FromString("psi-shell"), NativeProcedure.Create<ProcessStartInfo, bool, object>((psi, s) => psi.UseShellExecute = s) },
-                { Symbol.FromString("psi-createnowindow"), NativeProcedure.Create<ProcessStartInfo, bool, object>((psi, s) => psi.CreateNoWindow = s) },
-                { Symbol.FromString("psi-redirect-stdout"), NativeProcedure.Create<ProcessStartInfo, bool, object>((psi, s) => psi.RedirectStandardOutput = s) },
-                { Symbol.FromString("psi-redirect-stderr"), NativeProcedure.Create<ProcessStartInfo, bool, object>((psi, s) => psi.RedirectStandardError = s) },
-
-                { Symbol.FromString("ps-new"), NativeProcedure.Create(() => {
-                    var p = new Process();
-                    return p;
-                }) },
-                { Symbol.FromString("ps-start"), NativeProcedure.Create<string, string, Process>((cmd, arg) => {
-                    var p = Process.Start(cmd,arg);
-                    return p;
-                }) },
-
-                { Symbol.FromString("ps-psi"), NativeProcedure.Create<Process, ProcessStartInfo>(p => p.StartInfo) },
-
-                { Symbol.FromString("ps-interact"), NativeProcedure.Create<Process,object>((p) => {
-                    InteractProcess(p);
-                    return None.Instance;
-                })},
                 { Symbol.FromString("ps-wait"), NativeProcedure.Create<Process, int>(p => {
                     p.WaitForExit();
                     return p.ExitCode;
@@ -197,7 +104,6 @@ namespace Rll
             };
             var itpl = new Interpreter(new[] { appExt });
             return itpl;
-
         }
 
         public static void RunRepl()
